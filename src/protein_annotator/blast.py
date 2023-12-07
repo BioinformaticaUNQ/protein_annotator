ce165d74ad8181a61e275986b4e5b2083437eae5
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2023-12-06 23:08:08 -0300

    Move blast interactions to its own module

---
+++ src/protein_annotator/blast.py
@@ -0,0 +1,54 @@
+import logging
+from io import StringIO
+
+from Bio.Blast import NCBIWWW, NCBIXML
+from Bio.Blast.Applications import NcbiblastpCommandline
+from Bio.Blast.Record import Blast
+
+from protein_annotator.schemas import Args, Hit
+
+logger = logging.getLogger()
+
+
+def run_blast(args: Args) -> list[Hit]:
+    blastp_command = NcbiblastpCommandline(
+        db="../swissprot",
+        query=args.query,
+        # max_target_seqs=5,  # n hits
+        out="out.xml",
+        outfmt=5,  # Blast XML
+    )
+    logger.info(f"Executing command: {blastp_command}")
+
+    stdout, stderr = blastp_command()  # noqa
+
+    # Versión web, se queda esperando y nunca termina
+    # blast_result = NCBIWWW.qblast(
+    #     "blastp",
+    #     args.db,
+    #     parsed_input.protein.sequence,
+    #     threshold=40,
+    #     hitlist_size=5,
+    #     format_type="XML",
+    # )
+
+    query_result = ""
+    with open("out.xml", "r") as f:
+        file = f.read()
+        query_result = StringIO(file)
+
+    blast: Blast = NCBIXML.read(query_result)
+
+    hits = []
+    for alignment in blast.alignments:
+        for hsp in alignment.hsps:
+            hits.append(
+                Hit(
+                    description=alignment.hit_def,
+                    sequence=hsp.sbjct,
+                    coverage=len(hsp.sbjct) / len(hsp.query) * 100,
+                    e_value=hsp.expect,
+                    id_percentage=round(hsp.identities / hsp.align_length, 2) * 100,
+                )
+            )
+    return hits
