c2382f5f3f3f20148a9b7bb9b30d58d15014e1da
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2023-12-13 06:29:52 -0300

    Remove unused code

--- src/protein_annotator/annotations/uniprot.py
+++ src/protein_annotator/annotations/uniprot.py
@@ -1,62 +1,6 @@
-# import logging
 from typing import Any, Dict, List
 
 from protein_annotator.annotations.dbs import get_protein_from_db
-
-# logger = logging.getLogger()
-
-
-# def get_protein_api(uniprod_id: str) -> Record:
-#     response = httpx.get(f"https://rest.uniprot.org/uniprotkb/{uniprod_id}?format=txt")
-#     record = SwissProt.read(StringIO(response.text))
-#     return record
-
-
-# def get_protein_db(uniprod_id: str, path_db: str) -> SeqRecord:
-#     found_record = None
-#     with gzip.open(path_db, "rb") as handle:
-#         try:
-#             records: SwissIterator = SeqIO.parse(handle, "swiss")
-#             found_record = next(
-#                 (record for record in records if record.id == uniprod_id), None
-#             )
-#             return found_record
-#         except Exception:
-#             return found_record
-
-
-# def download_file(path_to_download: str, url: str, file_name: str) -> pathlib.Path:
-#     file_path = pathlib.Path(path_to_download) / file_name
-#     if file_path.exists():
-#         raise ValueError("File already exists")
-#     if file_path.is_dir():
-#         raise ValueError("File name must not be a directory")
-
-#     try:
-#         with open(file_path, "wb") as file_out:
-#             with httpx.stream(
-#                 method="GET", url=url, follow_redirects=True, timeout=60
-#             ) as response:
-#                 response.raise_for_status()
-
-#                 file_size = int(response.headers.get("Content-Length", 0))
-#                 desc = f"Downloading: {url}"
-
-#                 for chunk in tqdm(
-#                     iterable=response.iter_raw(1),
-#                     desc=desc,
-#                     unit="B",
-#                     unit_scale=True,
-#                     unit_divisor=1024,
-#                     total=file_size,
-#                 ):
-#                     file_out.write(chunk)
-
-#         return file_path
-#     except Exception as e:
-#         logger.exception(f"Error while downloading file {url}")
-#         file_path.unlink()
-#         raise e
 
 
 def annotate_site_uniprot(
